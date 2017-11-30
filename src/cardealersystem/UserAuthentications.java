a20c3d43216162bf085d8ca1bd6fb64163e34f32
Kevin <km031695@gmail.com>
2017-11-29 21:57:10 -0600

    added user authentication class

---
+++ src/cardealersystem/UserAuthentications.java
@@ -0,0 +1,19 @@
+package cardealersystem;
+
+public class UserAuthentications {
+
+    //scanner or buffered reader for users.txt
+
+
+    public void addNewUser(String username, String pw){
+        // VBadd info to users.txt
+    }
+
+    public void verifyUser(String username, String pw){
+        //check users.txt for user and pw
+    }
+
+    private void removeUser(String username, String pw){
+        //remove user from users.txt
+    }
+}
